c820a728890dc3b2a52fd304f6a968d552ee8fe1
TeeKay <takudzwamzembegwa@gmail.com>
2021-08-07 22:59:18 +0200

    made use of short-circuit and useEffect clean up

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => window.removeEventListener;
+  }, []);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      {" "}
+      <h1>window</h1>
+      <h2>size:{size} PX</h2>
+    </div>
+  );
+};
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <button className="btn" onClick={() => setShow(!show)}>
+        Show/Hide
+      </button>
+      {show && <Item />}
+    </>
+  );
 };
 
 export default ShowHide;
